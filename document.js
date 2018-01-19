7705794fc0443e239f947f55d60638f0bc3b86d1
mca380 <mcanovas@brujula.es>
2018-01-19 13:01:05 +0100

    canvis2

--- document.js
+++ document.js
@@ -12,3 +12,24 @@ function loadJSON(callback) {
     };
     xobj.send(null);  
 }
+function viewJSON() {
+    loadJSON(function(response) {
+        /* Aqui l'objecte response representa l'objecte JSON que ens 
+           ha retornat el servidor */
+        var llista = "";
+        for (i in response.results) {
+            var restaurant = response.results [i];
+            var restaurantS = "";
+            for (j in restaurant.types) {
+                restaurantS = restaurantS + restaurant.types [j] + ",";
+            }
+            llista = llista + "<h1>" + restaurant.name + "<img src = ' "+restaurant.icon+"'>" + "</h1> <br>"
+            + "Direcció: "+ restaurant.vicinity + "<br>"
+            + "Latitud: " + restaurant.geometry.location.lat + "<br>"
+            + "Longitud: "+ restaurant.geometry.location.lng + "<br>"
+            + "Tipus: " + restaurantS + "<br>";
+        }
+        document.getElementById("results").innerHTML = llista;
+    });
+}
+
